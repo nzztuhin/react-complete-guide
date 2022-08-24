51e00fadd56ea6f8d0fc234c4395bf9cd84c86bf
nzztuhin <64777785+nzztuhin@users.noreply.github.com>
2022-08-25 00:25:43 +0600

    life cycle hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,10 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
   state = {
     persons: [
       { id:'asfa1',  name: 'Max', age: 28 },
@@ -14,6 +18,16 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps',props);
+    return state;
+  }
+  componentWillUnmount(){
+    console.log('[App.js] componentWillMount');
+  }
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = (event, id) => {
@@ -42,6 +56,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
     
    if(this.state.showPersons){
