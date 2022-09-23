68b4c8c9ba09772faa8f2ed37a765c589c484c91
nzztuhin <64777785+nzztuhin@users.noreply.github.com>
2022-09-24 05:47:03 +0600

    added

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,11 +23,20 @@ class App extends Component {
     console.log('[App.js] getDerivedStateFromProps',props);
     return state;
   }
-  componentWillUnmount(){
-    console.log('[App.js] componentWillMount');
-  }
+  // componentWillUnmount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
   componentDidMount(){
     console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
